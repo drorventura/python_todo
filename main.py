109471233ad0f50a27a14e7be22cee5a3640aea6
dpeco <demetrius.pecoraro@sonatafy.com>
2024-08-15 09:39:02 -0300

    (feat) Integrating with mongodb using pymongo and setup the docker

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-from fastapi import FastAPI, Request
+import asyncio
+
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import json
 
+from warmly_repository import put_activities, delete_activity, bulk_insert, get_activities, get_last_id, \
+    get_collection
+
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
+
+
+@app.on_event("startup")
+def on_startup():
+    model = get_collection()
+    if model.count_documents({}) == 0:
+        with open('database.json') as f:
+            data = json.load(f)
+            documents = [{"id": int(key), "description": value} for key, value in data.items()]
+            bulk_insert(documents)
+
 
 @app.get("/")
 async def root(request: Request):
-    with open('database.json') as f:
-        data = json.load(f)
-    return templates.TemplateResponse("todolist.html",{"request":request,"tododict":data})
+    loop = asyncio.get_running_loop()
+    try:
+        data = await loop.run_in_executor(None, get_activities)
+        return templates.TemplateResponse("todolist.html", {"request": request, "tododict": data})
+    except asyncio.CancelledError:
+        return {"error": "Request was cancelled before completion"}
+    except Exception as e:
+        return {"error": f"An error occurred: {str(e)}"}
+
 
 @app.get("/delete/{id}")
 async def delete_todo(request: Request, id: str):
-    with open('database.json') as f:
-        data = json.load(f)
-    del data[id]
-    with open('database.json','w') as f:
-        json.dump(data,f)
+    delete_activity(id)
     return RedirectResponse("/", 303)
 
+
 @app.post("/add")
-async def add_todo(request: Request):
+async def add_todo(newtodo: str = Form(...)):
+    last_id = get_last_id()
+    new_id = last_id + 1
+    new_activity = {"id": new_id, "description": newtodo}
+    inserted_id = put_activities(new_activity)
+    return RedirectResponse("/", 303)
+
+
+@app.get("/migrate")
+async def migrate(request: Request):
     with open('database.json') as f:
         data = json.load(f)
-    formdata = await request.form()
-    newdata = {}
-    i=1
-    for id in data:
-        newdata[str(i)] = data[id]
-        i+=1
-    newdata[str(i)] = formdata["newtodo"]
-    print(newdata)
-    with open('database.json','w') as f:
-        json.dump(newdata,f)
+        documents = [{"id": int(key), "description": value} for key, value in data.items()]
+        result = bulk_insert(documents)
     return RedirectResponse("/", 303)
---
+++ warmly_repository.py
@@ -0,0 +1,43 @@
+﻿from bson import ObjectId
+from pymongo import MongoClient, DESCENDING
+
+client = MongoClient("mongodb://mongo:27017")
+db = client["warmly"]
+collection = db["activities"]
+
+'''
+Method to retrieve documents from collection
+'''
+
+
+def get_activities():
+    return list(collection.find().sort("id", -1))
+
+
+def get_collection():
+    return collection
+
+'''
+Method to insert a document into collection
+'''
+
+
+def bulk_insert(activities):
+    result = collection.insert_many(activities)
+    return result.inserted_ids
+
+
+def delete_activity(id):
+    collection.delete_one({"_id": ObjectId(id)})
+
+
+def put_activities(activities):
+    collection.insert_one(activities)
+
+
+def get_last_id():
+    # Busca o documento com o maior valor de "id"
+    last_activity = collection.find_one(sort=[("id", DESCENDING)])
+    if last_activity:
+        return last_activity["id"]
+    return 0
